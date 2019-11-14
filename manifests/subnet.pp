778a296aec3bc4f92db7d6a046855929781d01e5
efoft <pajan@yandex.ru>
2019-11-14 16:07:20 +0300

    Version 1.1.0

--- manifests/subnet.pp
+++ manifests/subnet.pp
@@ -1,61 +1,30 @@
-# === Define dhcp::subnet ===
+# @summary
+# This module only supports subnet declaraion with pools. Pools allow to split network into several ranges with different settings.
 #
-# This module only supports subnet declaraion with pools. Pools allow to split network
-# into several ranges with different settings.
+# @param network           Subnetwork address.
+# @param netmask           Subnetwork mask.
+# @param router            IP address of the network router.
+# @param domain            Domain name to assign to clients.
+# @param dns_servers       Array of IP addresses of DNS servers. If ddns is used then first element is array must be primary DNS server.
+# @param netbios_servers   Optional. Array of addresses of WINS-servers (if used).
+# @param failover          If this subnet must be included into cluster configuration.
+# @param failover_cluster  The name of cluster, fetched from main class.
+# @param ddns              Boolean to say if DDNS updates are used for this subnet.
+# @param ddns_zones        Array of zones that are served by the BIND server which are to be updated.
+# @param ddns_primary      Reassign used by default first item of *dns_servers* array.
+# @param ddns_key_name     Name of the rndc key, fetched from main class.
 #
-# === Parameters ===
-# [*network*]
-#   Subnetwork address.
-#
-# [*netmask*]
-#   Subnetwork mask.
-#
-# [*router*]
-#   IP address of the network router.
-#
-# [*domain*]
-#   Domain name to assign to clients.
-#
-# [*dns_servers*]
-#   Array of IP addresses of DNS servers. If ddns is used then first element is array must be primary DNS server.
-#
-# [*netbios_servers*]
-#   Optional. Array of addresses of WINS-servers (if used).
-#
-# [*failover*]
-#   If this subnet must be included into cluster configuration.
-#
-# [*failover_cluster*]
-#   The name of cluster, fetched from main class.
-#
-# [*ddns*]
-#   Boolean to say if DDNS updates are used for this subnet.
-#
-# [*ddns_zones*]
-#   Array of zones that are served by the BIND server which are to be updated.
-#   Default: zone with the same name as *domain*
-#
-# [*ddns_primary*]
-#   Reassign used by default first item of *dns_servers* array.
-#
-# [*ddns_key_name*]
-#   Name of the rndc key, fetched from main class.
-#   Default: rndc-key
-#
-# [*next_server*]
+# @param next_server
 #   IP address of pxe boot server where boot image resides. Can be defined on the subnet level (here)
 #   or otherwise be specific for each pool and defined in a pool.
 #
-# [*filename*]
+# @param filename
 #   The name of boot image for pxe. Can be defined on the subnet level (here)
 #   or otherwise be specific for each pool and defined in a pool.
-#   Default: pxelinux.0
 #
-# [*ttl*]
-#   Default lease time and max lease time are set to this value.
-#   Default: 43200 sec
+# @param ttl #   Default lease time and max lease time are set to this value.  #   Default: 43200 sec
 #
-# [*pools*]
+# @param pools
 #   Hash must contain the following keys:
 #   - start: first IP of the range
 #   - end:   last IP of the range
@@ -67,22 +36,22 @@
 #   - ttl
 #
 define dhcp::subnet (
-  Stdlib::Ip::Address $network,
-  Stdlib::Ip::Address $netmask,
-  Stdlib::Ip::Address $router,
-  String $domain,
-  Array[Stdlib::Ip::Address] $dns_servers,
-  Array[Stdlib::Ip::Address] $netbios_servers  = [],
-  Boolean $failover                            = $dhcp::failover,
-  String $failover_cluster                     = $dhcp::failover_cluster,
-  Boolean $ddns                                = false,
-  Optional[Array[String]] $ddns_zones          = undef,
-  Optional[Stdlib::Ip::Address] $ddns_primary  = undef,
-  String $ddns_key_name                        = $dhcp::ddns_key_name,
-  Optional[Stdlib::Ip::Address] $next_server   = undef,
-  String $filename                             = $dhcp::params::filename,
-  Numeric $ttl                                 = 43200,
-  Hash $pools,
+  Stdlib::Ip::Address           $network,
+  Stdlib::Ip::Address           $netmask,
+  Stdlib::Ip::Address           $router,
+  String                        $domain,
+  Array[Stdlib::Ip::Address]    $dns_servers,
+  Array[Stdlib::Ip::Address]    $netbios_servers  = [],
+  Boolean                       $failover         = $dhcp::failover,
+  String                        $failover_cluster = $dhcp::failover_cluster,
+  Boolean                       $ddns             = false,
+  Optional[Array[String]]       $ddns_zones       = undef,
+  Optional[Stdlib::Ip::Address] $ddns_primary     = undef,
+  String                        $ddns_key_name    = $dhcp::ddns_key_name,
+  Optional[Stdlib::Ip::Address] $next_server      = undef,
+  String                        $filename         = $dhcp::params::filename,
+  Numeric                       $ttl              = 43200,
+  Hash                          $pools,
 ) {
 
   $_ddns_zones = $ddns_zones ?
